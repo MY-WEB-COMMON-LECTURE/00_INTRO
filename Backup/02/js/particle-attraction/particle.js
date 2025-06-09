7542a7430f0a9f77c76152fe32d754627d609d08
JungWooGyun <jwg8910@naver.com>
2025-06-09 17:48:23 +0900

    ddd

--- Backup/02/js/particle-attraction/particle.js
+++ Backup/02/js/particle-attraction/particle.js
@@ -1,17 +1,38 @@
 function Particle(x,y){
-	this.pos = createVector(x,y);
-	this.prev = createVector(x,y);
+	// 위치를 100vw, 100vh 기준으로 넓게 랜덤 배치
+	this.pos = createVector(
+		(typeof x === 'number' ? x : random(windowWidth)),
+		(typeof y === 'number' ? y : random(windowHeight))
+	);
+	this.prev = createVector(this.pos.x, this.pos.y);
 	this.vel = createVector();
 	this.acc = createVector();
-	this.w = 5;
-	this.color1 = random(255);
-	this.color2 = random(255);
-	this.color3 = random(255);
+	// 크기: 아주작은(1~2), 조금작은(2.1~3), 중간작은(3.1~4)
+	var sizeRand = random();
+	if (sizeRand < 0.5) this.w = random(1, 2);           // 아주작은
+	else if (sizeRand < 0.85) this.w = random(2.1, 3);   // 조금작은
+	else this.w = random(3.1, 4);                        // 중간작은
+	// 검정 배경에 잘 보이는 쨍한 색상 팔레트
+	var colorPalette = [
+		'#00ffff', // cyan
+		'#ff00ff', // magenta
+		'#ffff00', // yellow
+		'#ff8000', // orange
+		'#00ff00', // lime
+		'#ff0000', // red
+		'#00aaff', // blue
+		'#ffffff', // white
+		'#ffd700', // gold
+		'#39ff14', // neon green
+		'#ff1493', // deep pink
+		'#1e90ff'  // dodger blue
+	];
+	this.color = colorPalette[Math.floor(random(colorPalette.length))];
 
 	this.show = function(){
-		stroke('#adeed7');
-		strokeWeight(1);
-		point(this.pos.x,this.pos.y);
+		noStroke();
+		fill(this.color);
+		ellipse(this.pos.x, this.pos.y, this.w, this.w);
 	}
 
 	this.update = function(){
